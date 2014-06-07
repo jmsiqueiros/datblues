d74a75dd9ed7008f1a60d7ea173c219a113393cb
Rodrigo García <absorto@sdf.org>
2014-06-07 14:11:49 -0500

    bluesmen as library, pull them

--- example_url_lib.py
+++ example_url_lib.py
@@ -1,7 +1,11 @@
 import urllib2
 
 
-artista = "Chris_Martin"
-response = urllib2.urlopen('http://en.wikipedia.org/w/api.php?format=json&action=query&titles='+artista+'&prop=revisions&rvprop=content')
-html = response.read()
-print "Get all data: ", html
+from bluesmen import bluesmen
+
+for bluesman in bluesmen:
+    response = urllib2.urlopen('http://en.wikipedia.org/w/api.php?format=json&action=query&titles='+bluesman+'&prop=revisions&rvprop=content')
+    html = response.read()
+
+    with open( bluesman+'.json', 'w') as f:
+        f.write( "Get all data: ", html)
